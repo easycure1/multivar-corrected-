1f9064e6697a28a658669e05185611aaf3e0d24a
easycure1 <45089175+easycure1@users.noreply.github.com>
2021-04-22 14:46:48 +1200

    Update Run_cpp.R

--- Run_cpp.R
+++ Run_cpp.R
@@ -1,4 +1,4 @@
 library(Rcpp)
 library(RcppArmadillo)
 
-sourceCpp('C:/Users/yliu894/Documents/Multi_corrected_R/proptotype.cpp')
+sourceCpp('C:/Users/yliu894/Documents/Multi_corrected_R/prototype.cpp')
